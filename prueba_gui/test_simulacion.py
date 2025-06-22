0a3dd506cd2590031c185b0a747a1fb80ac4dade
Imanol <ipalacios@uade.edu.ar>
2025-06-22 17:05:37 -0300

    Correccion en test_matriz

--- prueba_gui/test_simulacion.py
+++ prueba_gui/test_simulacion.py
@@ -1,14 +1,6 @@
-import unittest
 from simulacion_de_datos import crea_matriz
 
-#para comprobar si la matriz se crea correctamente abrir carpeta de prueba_gui y ejecutar python "test_simulacion.py" en la terminal
-
-class TestSimulacionDeDatos(unittest.TestCase):
-    def test_crea_matriz_es_4x4(self):
-        matriz = crea_matriz()
-        self.assertEqual(len(matriz), 4)  # Verifica 4 filas
-        for fila in matriz:
-            self.assertEqual(len(fila), 4)  # Verifica 4 columnas por fila
-
-if __name__ == "__main__":
-    unittest.main()
+def test_matriz():
+    matriz = crea_matriz()
+    assert len(matriz) == 4  #verifica que hay 4 filas
+    assert all(len(fila) == 4 for fila in matriz)  #verifica que cada fila tenga 4 columnas
