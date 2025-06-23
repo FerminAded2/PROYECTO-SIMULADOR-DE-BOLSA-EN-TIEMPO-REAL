e418883e75616c8ef058316f2a5eaefaad3102c9
Imanol Palacios Alvarez <ipalacios@uade.edu.ar>
2025-06-23 04:37:24 -0300

    Update test_simulacion.py
    corrección en test de largo de matriz.

--- prueba_gui/test_simulacion.py
+++ prueba_gui/test_simulacion.py
@@ -2,5 +2,6 @@ from simulacion_de_datos import crea_matriz
 
 def test_matriz():
     matriz = crea_matriz()
-    assert len(matriz) == 4  #verifica que hay 4 filas
-    assert all(len(fila) == 4 for fila in matriz)  #verifica que cada fila tenga 4 columnas
+    assert len(matriz) == 4  # verifica que hay 4 filas
+    for fila in matriz:
+        assert len(fila) == 4  # verifica que cada fila tenga 4 columnas
